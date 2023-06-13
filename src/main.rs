cfed899f0db11cfd3b492ddc2105b331390b67ca
Niclas Klugmann <klugmann@mail.uni-paderborn.de>
2023-06-13 17:28:44 +0200

    WIP

--- src/main.rs
+++ src/main.rs
@@ -1,16 +1,32 @@
 use clap::Parser;
-use std::path::PathBuf;
+use std::{
+    path::PathBuf,
+    io::Write
+};
 
 #[derive(Parser, Debug)]
-#[command(author, version, about, long_about = None)]
+#[command(author, version, about)]
 struct Args {
-    #[arg(short, long)]
-    option: Option<String>,
+    input_file: PathBuf,
+
+    #[arg(short)]
+    output_file: Option<PathBuf>,
 }
 
 fn main() {
-    let _args = Args::parse();
-    let _input_file_name = PathBuf::new();
-    let _output_file_name = PathBuf::new();
-    let _command = std::env::current_exe().expect("should get exe name!");
+    let args = Args::parse();
+    let mut output: Box<dyn Write> = args.output_file
+        .map(|x| Box::new(std::fs::File::open(x).unwrap()))
+        .unwrap_or_else(|| Box::new(std::io::stdout()));
+
+    if let Some(output_file) = args.output_file {
+        eprintln!("Output file chosen: {}", output_file.display());
+    } else {
+        eprintln!("Output to terminal");
+    };
+    write!(&mut output, "Input file: {}", args.input_file.display());
+
+    let full_command = std::env::current_exe().expect("Should get exe name!");
+    let command = full_command.file_name();
+    write!(&mut output, "{command:?}");
 }
